e4019e47626c7936eadf716f320c8d3521855c53
Sierra Masrh <10617333@my.uvu.edu>
2020-02-03 19:48:44 -0700

    If break everyting remove scene2020

---
+++ Practice/Assets/CharacterTalk.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterTalk : MonoBehaviour {
+    public Queue<string> words;
+
+	// Use this for initialization
+	void Start () {
+        words = new Queue<string>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Practice/Assets/Word.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class Word : MonoBehaviour {
+
+    public string greeting;
+    public string[] speach;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
