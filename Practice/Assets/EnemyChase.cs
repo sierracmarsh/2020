a89dca00ea2b367e67407233f14cdbb9a39ce435
Sierra Masrh <10617333@my.uvu.edu>
2020-02-09 20:43:12 -0700

    Enemy

---
+++ Practice/Assets/EnemyChase.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyChase : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        chased = GameObject.FindGameObjectWithTag("Player").transform;
+        NMAgent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float distance = Vector3.Distance(tranform.position, chased.position);
+        if(distance > 1 && !isdead)
+        {
+            NMAgent.updatePosition = true;
+            NMAgent.SetDestination(target.position);
+            anim.SetBool("walk", true);
+            anim.SetBool("Chase", false);
+
+
+        }
+        else
+        NMAgent.updatePosition = false;
+        anim.SetBool("walk", false);
+        anim.SetBool("Chase", true);
+
+
+    }
+}
