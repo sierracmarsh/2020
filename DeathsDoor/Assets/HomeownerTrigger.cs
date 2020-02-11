c0e86ec1fb3bea5c1913fa4a1dea5127e9cd4928
Sierra Masrh <10617333@my.uvu.edu>
2020-02-10 19:17:18 -0700

    Homeowner

---
+++ DeathsDoor/Assets/HomeownerTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HomeownerTrigger : MonoBehaviour
+{
+    public GameObject Homeowner;
+    public float speed;
+
+
+    void OnTriggerEnter(Collider col)
+    {
+
+        if (col.gameObject.tag == "Player")
+        {
+            Homeowner.transform.Translate(new Vector3(8, 0, 0) * Time.deltaTime);
+
+        }
+    }
+}
