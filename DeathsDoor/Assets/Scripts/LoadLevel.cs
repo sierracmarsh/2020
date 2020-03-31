8fc5d1536afba91a5592138c00cf1fc214a6912b
Sierra Masrh <10617333@my.uvu.edu>
2020-03-31 05:34:46 -0600

    Revert "MIsc"
    This reverts commit f5ef56ffb8f18e46f68df6bdfe139dd687d8ae26.

---
+++ DeathsDoor/Assets/Scripts/LoadLevel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.name == "Tim")
+		{
+			SceneManager.LoadScene(3);
+		}
+	}
+}
