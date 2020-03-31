e7f88bf9181157ce04e27856c4a1ea62c4a275b3
Sierra Masrh <10617333@my.uvu.edu>
2020-03-31 07:28:42 -0600

    CandyCount

---
+++ DeathsDoor/Assets/Scripts/CandyCollect.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CandyCollect : MonoBehaviour
+{
+	public AudioSource Wrapper;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		Wrapper.Play();
+		ScoreSystem.count += 1;
+		Destroy(gameObject);
+
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ DeathsDoor/Assets/Scripts/ScoreSystem.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreSystem : MonoBehaviour
+{
+    public GameObject CandyCount;
+    public static int count;
+ 
+
+    void Update()
+    {
+       
+        CandyCount.GetComponent<Text>().text = "Candy Count: " + count;
+    }
+
+}
