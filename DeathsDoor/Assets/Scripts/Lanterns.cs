b2ff2286be88f94c6f2b90738c8e00da21edb4c7
Sierra Masrh <10617333@my.uvu.edu>
2020-03-31 08:00:33 -0600

    Lat

---
+++ DeathsDoor/Assets/Scripts/Lanterns.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lanterns : MonoBehaviour {
+
+	
+		private bool away = false;
+		private float awayspeed = 0;
+	
+
+		void OnTriggerEnter(Collider touch)
+		{
+			if (touch.gameObject.name == "Tim")
+			{
+				away = true;
+			
+			}
+		}
+
+		void Update()
+		{
+			if (away)
+			{
+				awayspeed += Time.deltaTime;
+				transform.position = new Vector3 (transform.position.x, transform.position.y - awayspeed,
+					transform.position.z);
+				
+			}
+		}
+		
+	}
+
+
