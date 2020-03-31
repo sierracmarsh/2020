3f52de6a0f4c60f9b31e295062a4f1ea00db36ac
Sierra Masrh <10617333@my.uvu.edu>
2020-03-31 01:08:44 -0600

    Say  ToT

---
+++ DeathsDoor/Assets/Scripts/ToTText.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToTText : MonoBehaviour
+{
+	public GameObject UObj;
+	void Start () {
+		UObj.SetActive(false);
+	}
+
+
+	void OnTriggerEnter(Collider player)
+	{
+		if (player.gameObject.tag == "Player")
+		{
+			UObj.SetActive(true);
+			StartCoroutine("WaitforSec");
+		}
+	}
+
+	IEnumerator WaitforSec()
+	{
+		yield return new WaitForSeconds(2);
+		Destroy(UObj);
+		Destroy(gameObject);
+	}
+}
