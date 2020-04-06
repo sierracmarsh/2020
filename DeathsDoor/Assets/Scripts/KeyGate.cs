fed58d31dd668fe13a169e8dcca8f053aa80c4c2
Sierra Masrh <10617333@my.uvu.edu>
2020-04-05 23:20:20 -0600

    misc

--- DeathsDoor/Assets/Scripts/KeyGate.cs
+++ DeathsDoor/Assets/Scripts/KeyGate.cs
@@ -1,8 +1,23 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Collections;
 
 public class KeyGate : MonoBehaviour {
-v
+    
+    private void OnTriggerEnter(Collider other)
+    
+    {
+        if (other.gameObject.name == "Tim" && GameVariables.keyCount>0)
+        {
+            GameVariables.keyCount--;
+            Destroy(gameObject);
+           
+           
+
+            
+
+        }
+}
 }
