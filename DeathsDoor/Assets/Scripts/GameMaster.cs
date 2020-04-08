bc6c614d0df5204553fde99feda699d0a4a6af75
Sierra Masrh <10617333@my.uvu.edu>
2020-04-07 21:53:46 -0600

    GameMaster

---
+++ DeathsDoor/Assets/Scripts/GameMaster.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour
+{
+    public static GameMaster Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- DeathsDoor/Assets/Scripts/KeyGate.cs
+++ DeathsDoor/Assets/Scripts/KeyGate.cs
@@ -1,8 +1,8 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.Collections;
+
+
 
 public class KeyGate : MonoBehaviour {
     
